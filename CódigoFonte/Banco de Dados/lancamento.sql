6b5a3ac5ff4d7ce095503c7a72b8e3d6ca470d1a
marcoalvesalmeida <marco.ifnmg@gmail.com>
2017-12-16 08:42:12 -0200

    Adicionando SQL da tabela Lançamento #3

---
+++ CódigoFonte/Banco de Dados/lancamento.sql
@@ -0,0 +1,14 @@
+-- Criação da tabela Lançamento
+create table lancamento(
+    id int auto_increment PRIMARY KEY,
+    data_lancamento datetime,
+    tipolancamento varchar(30),
+    valor decimal(6,2),
+    acrescimo int,
+    desconto int,
+    valortotal decimal(6,2),
+    cliente int, -- Chave Estrangeira de Cliente
+    planejamento varchar(30),
+    formaPagamento varchar(30),
+    tipoPagamento varchar(30)
+);
